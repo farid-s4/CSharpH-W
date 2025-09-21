729e969ab90f4ef3f7e99d2cb6a1aff2cb0e3669
S FARID <faridsadirov4@gmail.com>
2025-09-21 20:57:10 +0400

    update

---
+++ Sync/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+
+var manualEvent = new EventWaitHandle(true, EventResetMode.ManualReset, "Global\\MyAppEvent");
+var manualEvent1 = new EventWaitHandle(false, EventResetMode.ManualReset, "Global\\MyAppEven1");
+
+for (int i = 0; i <= 10; i+=2)
+{
+    manualEvent.WaitOne();
+    manualEvent.Reset(); 
+    Console.WriteLine(i);
+    Thread.Sleep(1000);
+    manualEvent1.Set();
+}
---
+++ Sync/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+var manualEvent = new EventWaitHandle(false, EventResetMode.ManualReset, "Global\\MyAppEvent");
+var manualEvent1 = new EventWaitHandle(false, EventResetMode.ManualReset, "Global\\MyAppEven1");
+
+
+for (int i = 1; i <= 10; i+=2)
+{
+    manualEvent1.WaitOne();
+    manualEvent1.Reset(); 
+    Console.WriteLine(i);
+    Thread.Sleep(1000);
+    manualEvent.Set();
+}
